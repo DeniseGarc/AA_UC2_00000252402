80ad5b6e1643e49f6f567c3c98eaf731017289fc
DeniseGarc <aliciadenise0623@gmail.com>
2025-03-04 20:12:22 -0700

    Commit inicial: creación del proyecto AA_UC2_00000252402

---
+++ src/aa_uc2_00000252402/Pruebas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aa_uc2_00000252402;
+
+/**
+ *
+ * @author Alici
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
